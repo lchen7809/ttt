5cdf1e543ceb77b322357eb58153146e4db59f6c
lchen7809 <160735650+lchen7809@users.noreply.github.com>
2024-10-30 12:50:01 +0800

    take turns

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,18 @@ import Board from './components/Board';
 const App = () => {
   const [squares, setSquares] = useState(Array(3).fill(null).map(() => Array(3).fill(null)));
 
+  const [isXNext, setIsXNext] = useState(true);
+
   const handleSquareClick = (row, col) => {
+    if (squares[row][col]) return; 
+    
     const newSquares = squares.map((r, i) => r.slice());
-    newSquares[row][col] = 'X';
+    newSquares[row][col] = isXNext ? 'X' : 'O'; //take turns X or O
     setSquares(newSquares);
+    setIsXNext(!isXNext);
   };
+  
+  
 
   return (
     <div className="app">
