8f3a2f5d39f96ee040c25982e74e7e7941fa5aa4
lchen7809 <160735650+lchen7809@users.noreply.github.com>
2024-10-30 12:58:18 +0800

    testing winner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ const App = () => {
 
   const [isXNext, setIsXNext] = useState(true);
 
+  const [winner, setWinner] = useState(null);
+
   const handleSquareClick = (row, col) => {
     if (squares[row][col]) return; 
     
@@ -13,13 +15,54 @@ const App = () => {
     newSquares[row][col] = isXNext ? 'X' : 'O'; //take turns X or O
     setSquares(newSquares);
     setIsXNext(!isXNext);
-  };
-  
-  
 
+    const result = checkWinner(newSquares);
+    if (result) {
+      setWinner(result);
+    } else { //if no winner, continue
+      setIsXNext(!isXNext);
+      }
+  };
+
+
+  //testin checkWinner in frontend
+  const checkWinner = (board) => {
+
+    const lines = [
+      
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]],
+
+    ];
+
+
+    for (let line of lines) {
+      const [a, b, c] = line;
+      if (
+        board[a[0]][a[1]] &&
+        board[a[0]][a[1]] === board[b[0]][b[1]] &&
+        board[a[0]][a[1]] === board[c[0]][c[1]]
+      ) {
+        return board[a[0]][a[1]]; 
+      }
+    }
+
+    return null; 
+  };
+
+  
   return (
     <div className="app">
       <h1>Tic-Tac-Toe</h1>
+      ${winner}
       <Board squares={squares} onSquareClick={handleSquareClick} />
     </div>
   );
